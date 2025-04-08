532268534d9aa3de743182053fc55bf5ca98c46b
Johnson Zhao <johnsonzhao275@gmail.com>
2025-04-08 16:27:57 -0400

    homepage wip

---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <section className="text-center mt-20">
+            <h2 className="text-4xl font-bold">Hi, I'm Jane Doe</h2>
+            <p className="mt-4 text-lg">A Front-End Developer passionate about React and design.</p>
+        </section>
+    )
+}
