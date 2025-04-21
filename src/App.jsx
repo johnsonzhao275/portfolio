ceacc91df1df6869165482e747c7b0ba6cb073c2
Johnson Zhao <johnsonzhao275@gmail.com>
2025-04-21 15:23:23 -0400

    configure server to handle client-side routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import NavigationBar from "./components/NavigationBar";
 import Home from "./pages/Home";
 import About from "./pages/About";
